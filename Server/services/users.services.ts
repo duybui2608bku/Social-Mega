200658e186482e82fb2dbb0a21cf518efb6ba800
Bui Nhat Duy <duy.bui2602@hcmut.edu.vn>
2024-07-15 23:01:50 +0700

    user-service

--- Server/services/users.services.ts
+++ Server/services/users.services.ts
@@ -1,15 +1,40 @@
+import { RegisterRequestBody } from '~/models/requestes/User.requests'
 import databaseService from '../services/database.services'
 import User from '../src/models/schemas/User.schema'
+import { hashPassword } from '~/utils/crypro'
+import signToken from '~/utils/jwt'
+import { TokenType } from '~/constants/enum'
 class UsersService {
-  async register(payload: { email: string; password: string }) {
-    const { email, password } = payload
-    const results = await databaseService.users.insertOne(
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.AccessToken },
+      options: { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN, algorithm: 'HS256' }
+    })
+  }
+
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: { user_id, token_type: TokenType.AccessToken },
+      options: { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN, algorithm: 'HS256' }
+    })
+  }
+
+  async register(payload: RegisterRequestBody) {
+    const result = await databaseService.users.insertOne(
       new User({
-        email,
-        password
+        ...payload,
+        date_of_birth: new Date(payload.date_of_birth),
+        password: hashPassword(payload.password)
       })
     )
-    return results
+
+    const user_id = result.insertedId.toHexString()
+    const [access_token, refresh_token] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+
+    return { access_token, refresh_token }
   }
   async checkEmail(email: string) {
     const user = await databaseService.users.findOne({ email })
